501ed295ce2df0beaa6194f315d00d17fc33a2ba
Rinigus <rinigus.git@gmail.com>
2017-03-05 12:22:02 +0200

    add qtsql to travis

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y \
      libprotobuf-dev protobuf-compiler \
      libagg-dev libfreetype6-dev \
      libcairo2-dev libpangocairo-1.0-0 libpango1.0-dev \
-     qt5-default qtdeclarative5-dev libqt5svg5-dev qtlocation5-dev libqt5sql5 \
+     qt5-default qtdeclarative5-dev libqt5svg5-dev qtlocation5-dev libqt5sql5-dev \
      freeglut3 freeglut3-dev \
      libmarisa-dev libmicrohttpd-dev libsnappy-dev libsqlite3-dev
 
