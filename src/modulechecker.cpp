3aa6d1b7850b2743e320b1094d43e3143381b332
Rinigus <rinigus.git@gmail.com>
2017-05-21 15:38:48 +0300

    make mapnik-dependent module check optional

--- src/modulechecker.cpp
+++ src/modulechecker.cpp
@@ -4,10 +4,12 @@
 
 ModuleChecker::ModuleChecker(QObject *parent) : QObject(parent)
 {  
+#ifdef USE_MAPNIK
   { // fonts module
     QFileInfo f(MAPNIK_FONTS_DIR);
     m_fonts = f.exists();
   }
+#endif
 
   m_valhalla_route = true;
 }
