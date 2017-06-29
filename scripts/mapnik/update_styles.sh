c48b48a4538e65e9c241c8b3f389ee634f81803e
Rinigus <rinigus.git@gmail.com>
2017-06-29 23:06:48 +0300

    update mapnik styles

--- scripts/mapnik/update_styles.sh
+++ scripts/mapnik/update_styles.sh
@@ -23,8 +23,16 @@ rm -rf "$STYLE"
 sync_style () {
     mkdir -p $2
     rsync -av --exclude "*.mss" --exclude "*.mml" $1 $2
+    (cd $2 && ln -s ../../res/icons .)
+    (cd $2 && ln -s ../../res/img .)
+    (cd $2 && ln -s ../../res/shield-day .)
+    (cd $2 && ln -s ../../res/shield-night .)
 }
 
 sync_style "$SCRIPTPATH/mapnik-styles-sqlite/install/OSMBright/"          "$STYLE"/default/day
 sync_style "$SCRIPTPATH/mapnik-styles-sqlite/install/MidnightCommander/"  "$STYLE"/default/night
 
+sync_style "$SCRIPTPATH/mapnik-styles-sqlite/install/OSMBrightCar/"          "$STYLE"/car/day
+sync_style "$SCRIPTPATH/mapnik-styles-sqlite/install/MidnightCommanderCar/"  "$STYLE"/car/night
+
+rsync -av "$SCRIPTPATH/mapnik-styles-sqlite/res" "$STYLE"/
