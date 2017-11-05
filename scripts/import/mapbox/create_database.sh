bcac29cad4a13d15bee472767066d2761df5ca46
Rinigus <rinigus.git@gmail.com>
2017-11-05 16:47:52 +0200

    fix typos

--- scripts/import/mapbox/create_database.sh
+++ scripts/import/mapbox/create_database.sh
@@ -7,7 +7,7 @@ source $PROGPATH/env.sh
 
 createdb --host=$POSTGRES_HOST --port=$POSTGRES_PORT --user=$POSTGRES_USER $POSTGRES_DB
 
-psql --host=$POSTGRES_HOST --port=$POSTGRES_PORT --user=$POSTGRES_USER --dbname="$db" <<-'EOSQL'
+psql --host=$POSTGRES_HOST --port=$POSTGRES_PORT --user=$POSTGRES_USER --dbname="$POSTGRES_DB" <<-'EOSQL'
     CREATE EXTENSION postgis;
     CREATE EXTENSION hstore;
     CREATE EXTENSION unaccent;
