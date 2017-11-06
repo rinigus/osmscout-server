e1aa82bf646dbbb10895d6a6b8d509a2e37d5449
Rinigus <rinigus.git@gmail.com>
2017-11-06 22:26:59 +0200

    [wip] mapbox gl master

---
+++ src/mapboxglmaster.cpp
@@ -0,0 +1,52 @@
+#include "mapboxglmaster.h"
+
+#include "infohub.h"
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QVariant>
+
+MapboxGLMaster::MapboxGLMaster(QObject *parent) : QObject(parent)
+{
+  QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", const_conn_world);
+  db.setDatabaseName("world.mbtiles");
+  db.open();
+}
+
+MapboxGLMaster::~MapboxGLMaster()
+{
+
+}
+
+bool MapboxGLMaster::getTile(int x, int y, int z, QByteArray &result, bool &compressed, bool &found)
+{
+  compressed = true; /// maybe would be flexible in future, for now just assume its compressed
+
+  QSqlDatabase db_world = QSqlDatabase::database(const_conn_world, true);
+
+  if (!db_world.isOpen()) return false;
+
+  QSqlQuery query(db_world);
+  query.setForwardOnly(true);
+  query.prepare("SELECT tile_data FROM tiles WHERE (zoom_level=:z AND tile_column=:x AND tile_row=:y)");
+  query.bindValue(":x", x);
+  query.bindValue(":y", (1 << z) - 1 - y); // conversion between XYZ and TMS
+  query.bindValue(":z", z);
+
+  if (!query.exec())
+    {
+      InfoHub::logWarning(tr("Failed to run query in Mapbox GL database"));
+      return false;
+    }
+
+  while (query.next())
+    {
+      // will be called only once since there is only one tile matching it
+      result = query.value(0).toByteArray();
+      found = true;
+      return true;
+    }
+
+  found = false;
+  return true;
+}
---
+++ src/mapboxglmaster.h
@@ -0,0 +1,27 @@
+#ifndef MAPBOXGLMASTER_H
+#define MAPBOXGLMASTER_H
+
+#include <QByteArray>
+#include <QObject>
+#include <QString>
+
+class MapboxGLMaster : public QObject
+{
+  Q_OBJECT
+
+public:
+  explicit MapboxGLMaster(QObject *parent = nullptr);
+  virtual ~MapboxGLMaster();
+
+  bool getTile(int x, int y, int z, QByteArray &result, bool &compressed, bool &found);
+
+signals:
+
+public slots:
+
+protected:
+
+  const QString const_conn_world{"mapboxgl: world"};
+};
+
+#endif // MAPBOXGLMASTER_H
