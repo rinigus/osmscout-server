74ce53a29a787c21788d715248a8c5abd2757b6c
Rinigus <rinigus.git@gmail.com>
2018-07-12 20:24:22 +0300

    ci: drop valhalla until newer lz4 will be available in travis

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -5,7 +5,7 @@ set -e
 echo "Build start time: `date`"
 
 ln -s pro/osmscout-server_console.pro .
-qmake CONFIG+="disable_mapnik disable_systemd" osmscout-server_console.pro
+qmake CONFIG+="disable_mapnik disable_systemd disable_valhalla" osmscout-server_console.pro
 make
 
 echo "Build end time: `date`"
