bda8a6a03bebb727e755d4e18aaab6e8cf889542
Rinigus <rinigus.git@gmail.com>
2018-08-24 09:53:40 +0300

    update Valhalla import binaries

--- scripts/import/valhalla/import_planet.sh
+++ scripts/import/valhalla/import_planet.sh
@@ -10,13 +10,10 @@ cd $PROGPATH
 rm -rf tiles *.bin
 mkdir tiles
 
-install/valhalla_build_timezones valhalla.json
+install/bin/valhalla_build_timezones valhalla.json
 
-# should be set after timezones
-export LD_LIBRARY_PATH=$PROGPATH/install
-
-install/valhalla_build_admins --config valhalla.json $PLANET
-install/valhalla_build_tiles --config valhalla.json $PLANET
+install/bin/valhalla_build_admins --config valhalla.json $PLANET
+install/bin/valhalla_build_tiles --config valhalla.json $PLANET
 
 # gzip all tiles
 gzip -r tiles/?
