6aa394b42e84f8fc330bac44613fedfe5a277bad
Rinigus <rinigus.git@gmail.com>
2018-08-24 06:12:32 +0300

    update travis scripts

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -4,8 +4,29 @@ set -e
 
 echo "Build start time: `date`"
 
+CFG=""
+
+if [ -z ${OSMSCOUT+x} ];
+then
+    CFG+="disable_osmscout "
+fi
+if [ -z ${MAPNIK+x} ];
+then
+    CFG+="disable_mapnik "
+fi
+if [ -z ${SYSTEMD+x} ];
+then
+    CFG+="disable_systemd "
+fi
+if [ -z ${VALHALLA+x} ];
+then
+    CFG+="disable_valhalla "
+fi
+
+echo "Configuration:" $CFG
+
 ln -s pro/osmscout-server_console.pro .
-qmake CONFIG+="disable_mapnik disable_systemd disable_valhalla" osmscout-server_console.pro
+qmake CONFIG+="$CFG" osmscout-server_console.pro
 make
 
 echo "Build end time: `date`"
