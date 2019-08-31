0d1b246cf4ea34950989a9428314156521280e6c
Rinigus <rinigus.git@gmail.com>
2019-08-31 15:42:02 +0300

    check whether it is possible to set desktop file

--- src/main.cpp
+++ src/main.cpp
@@ -129,7 +129,9 @@ int main(int argc, char *argv[])
 
   app->setApplicationName(APP_NAME);
   app->setOrganizationName(APP_NAME);
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 7, 0))
   app->setDesktopFileName(APP_NAME ".desktop");
+#endif
   app->setApplicationVersion(APP_VERSION);
 
   {
