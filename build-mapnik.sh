e6d5f33626c97f8d5dfd8ed6b4346aa9ad831e24
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2020-01-17 00:31:08 +0100

    create directory before copying

--- build-mapnik.sh
+++ build-mapnik.sh
@@ -5,6 +5,8 @@ cp -r "${SRC_DIR}"/mapnik/* .
 
 make reset
 ./configure INPUT_PLUGINS="sqlite,shape" DESTDIR="${INSTALL_DIR}" PREFIX="${INSTALL_DIR}/usr" CUSTOM_CXXFLAGS="-fPIC -g0" CUSTOM_CFLAGS="-fPIC -g0" LINKING=shared OPTIMIZATION=2 CPP_TESTS=no CAIRO=no PLUGIN_LINKING=static MEMORY_MAPPED_FILE=no DEMO=no MAPNIK_INDEX=no MAPNIK_RENDER=no #ENABLE_STATS=True ENABLE_LOG=True
-make
+make -j$(nproc)
 make install
+
+mkdir -p "${INSTALL_DIR}/usr/include"
 cp -r deps/mapbox/variant/include/mapbox "${INSTALL_DIR}/usr/include"
