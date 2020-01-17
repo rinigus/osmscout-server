dbb58bee9cd1143e53c79a45a06f518ab0496ac2
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2020-01-17 21:00:49 +0100

    Fix desktop build

--- build-libpostal.sh
+++ build-libpostal.sh
@@ -8,9 +8,15 @@ CXXFLAGS="-fPIC"
 ./bootstrap.sh
 
 ARCH_FLAGS=""
-if [[ ${ARCH_TRIPLET} == "arm-linux-gnueabihf" ]]; then
-  ARCH_FLAGS="--with-cflags-scanner-extra=-marm"
-fi
+case ${ARCH_TRIPLET} in
+"arm-linux-gnueabihf" )
+    ARCH_FLAGS="--with-cflags-scanner-extra=-marm"
+    ;;
+"aarch64-linux-gnu" )
+    # TODO what do we need to pass here for arm64?
+    ARCH_FLAGS="--with-cflags-scanner-extra=-marm"
+    ;;
+esac
 
 ./configure "${ARCH_FLAGS}" --disable-sse2 --datadir="/usr/local/libpostal/data" --disable-data-download --enable-static --disable-shared --host="${ARCH_TRIPLET}"
 make -j$(nproc)
