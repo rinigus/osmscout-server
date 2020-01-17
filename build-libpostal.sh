037d0d6d685722d93b3b041a5ee17fbbc3ca5647
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2020-01-17 20:40:08 +0100

    enable libpostal build for amd64

--- build-libpostal.sh
+++ build-libpostal.sh
@@ -6,6 +6,12 @@ cp -r "${SRC_DIR}"/libpostal/* .
 CFLAGS="-fPIC -lstdc++"
 CXXFLAGS="-fPIC"
 ./bootstrap.sh
-./configure --with-cflags-scanner-extra=-marm --disable-sse2 --datadir="/usr/local/libpostal/data" --disable-data-download --enable-static --disable-shared --host="${ARCH_TRIPLET}"
+
+ARCH_FLAGS=""
+if [[ ${ARCH_TRIPLET} == "arm-linux-gnueabihf" ]]; then
+  ARCH_FLAGS="--with-cflags-scanner-extra=-marm"
+fi
+
+./configure "${ARCH_FLAGS}" --disable-sse2 --datadir="/usr/local/libpostal/data" --disable-data-download --enable-static --disable-shared --host="${ARCH_TRIPLET}"
 make -j$(nproc)
 make install DESTDIR="${INSTALL_DIR}"
