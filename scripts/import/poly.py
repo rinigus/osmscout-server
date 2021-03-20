54e2f6563954547fd33d7e2db654c3c1785d0461
Rinigus <rinigus.git@gmail.com>
2021-03-20 14:09:45 +0200

    convert python scripts to python3

--- scripts/import/poly.py
+++ scripts/import/poly.py
@@ -1,3 +1,4 @@
+import glob
 from shapely.geometry import MultiPolygon, Polygon
 
 # taken from http://wiki.openstreetmap.org/wiki/Osmosis/Polygon_Filter_File_Python_Parsing
@@ -12,41 +13,53 @@ def parse_poly(fname):
     coords = []
 
     lines = open(fname, 'r')
-    
+
     for (index, line) in enumerate(lines):
         if index == 0:
             # first line is junk.
             continue
-        
+
         elif index == 1:
             # second line is the first polygon ring.
             coords.append([[], []])
             ring = coords[-1][0]
             in_ring = True
-        
+
         elif in_ring and line.strip() == 'END':
             # we are at the end of a ring, perhaps with more to come.
             in_ring = False
-    
+
         elif in_ring:
             # we are in a ring and picking up new coordinates.
-            ring.append(map(float, line.split()))
-    
+            ring.append(list(map(float, line.split())))
+
         elif not in_ring and line.strip() == 'END':
             # we are at the end of the whole polygon.
             break
-    
+
         elif not in_ring and line.startswith('!'):
             # we are at the start of a polygon part hole.
             coords[-1][1].append([])
             ring = coords[-1][1][-1]
             in_ring = True
-    
+
         elif not in_ring:
             # we are at the start of a polygon part.
             coords.append([[], []])
             ring = coords[-1][0]
             in_ring = True
-    
+
     return MultiPolygon(coords)
 
+def intersects(areas, coors):
+    poly = Polygon( ( (coors[0], coors[1]), (coors[0], coors[3]),
+                      (coors[2], coors[3]), (coors[2], coors[1]) ) )
+    for a in areas:
+        if a.intersects(poly):
+            return True
+    return False
+
+if __name__ == '__main__':
+    area = [parse_poly("hierarchy/europe/estonia/poly")]
+    i = intersects(area, [25, 57, 26, 60])
+    print("intersects: ", i)
