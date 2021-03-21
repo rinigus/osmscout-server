255572e5f09b65581b9b3880351e37bafc98bad9
Rinigus <rinigus.git@gmail.com>
2021-03-21 12:12:22 +0200

    change DBus service, path, and interface names
    Use the same notation as in daemon branch

--- src/config.h
+++ src/config.h
@@ -46,12 +46,12 @@
 #define GENERAL_APP_VERSION 4
 
 // d-bus access
-#define DBUS_SERVICE "org.osm.scout.server1"
-#define DBUS_PATH_ROOT "/org/osm/scout/server1"
-#define DBUS_INTERFACE_ROOT "org.osm.scout.server1"
+#define DBUS_SERVICE "io.github.rinigus.OSMScoutServer"
+#define DBUS_PATH_ROOT "/io/github/rinigus/OSMScoutServer"
+#define DBUS_INTERFACE_ROOT "io.github.rinigus.OSMScoutServer"
 
-#define DBUS_PATH_MAPMATCHING DBUS_PATH_ROOT "/mapmatching1"
-#define DBUS_INTERFACE_MAPMATCHING DBUS_INTERFACE_ROOT ".mapmatching1"
+#define DBUS_PATH_MAPMATCHING DBUS_PATH_ROOT "/mapmatching"
+#define DBUS_INTERFACE_MAPMATCHING DBUS_INTERFACE_ROOT ".mapmatching"
 
 //////////////////////////////////////////////
 /// global variables
