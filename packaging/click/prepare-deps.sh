83e2b260ef0951fbb3307402dae31eca2e506d92
Rinigus <rinigus.git@gmail.com>
2021-03-29 22:27:13 +0300

    [ubuntu touch] set Valhalla version via release tag

--- packaging/click/prepare-deps.sh
+++ packaging/click/prepare-deps.sh
@@ -10,4 +10,4 @@ rm -rf "${LIBPOSTAL_SRC_DIR}" "${VALHALLA_SRC_DIR}"
 
 # Download sources
 git clone --recursive --shallow-submodules -b devel https://github.com/rinigus/pkg-libpostal.git "${LIBPOSTAL_SRC_DIR}"
-git clone --recursive -b 3.0 https://github.com/rinigus/pkg-valhalla-lite.git "${VALHALLA_SRC_DIR}"
+git clone --recursive -b 3.0.9 https://github.com/rinigus/pkg-valhalla-lite.git "${VALHALLA_SRC_DIR}"
