b96a433a3d3eb2b6dd97a285aa87320dc0b076cc
Jonatan Hatakeyama Zeidler <jonatan_zeidler@gmx.de>
2022-01-25 21:21:53 +0100

    Fix Clickable desktop mode

--- packaging/click/launch.sh
+++ packaging/click/launch.sh
@@ -2,11 +2,12 @@
 set -Eeuo pipefail
 
 SCRIPT_SRC=osmscout-server.conf
-SCRIPT_DST=/home/phablet/.config/upstart/osmscout-server.conf
+SCRIPT_DST=/home/phablet/.config/upstart/
 
 if [[ ! -f "${SCRIPT_DST}" ]]; then
+	mkdir -p "${SCRIPT_DST}"
 	cp "${SCRIPT_SRC}" "${SCRIPT_DST}"
-	start osmscout-server
+	start osmscout-server || true
 fi
 
 osmscout-server-gui
