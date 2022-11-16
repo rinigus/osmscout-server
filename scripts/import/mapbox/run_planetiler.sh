e6af4dbee7c6c23d01b3d2d3b77db1d8cebc4931
Rinigus <rinigus.git@gmail.com>
2022-11-16 22:58:28 +0200

    Add Planetiler run script

---
+++ scripts/import/mapbox/run_planetiler.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$(readlink -f "$0")")"
+cd planetiler
+
+# from https://github.com/onthegomap/planetiler/blob/main/PLANET.md
+
+echo java -Xmx110g \
+  `# return unused heap memory to the OS` \
+  -XX:MaxHeapFreeRatio=40 \
+  -jar planetiler.jar \
+  `# Download the latest planet.osm.pbf from s3://osm-pds bucket` \
+  --area=planet --bounds=planet --download \
+  `# Accelerate the download by fetching the 10 1GB chunks at a time in parallel` \
+  --download-threads=10 --download-chunk-size-mb=1000 \
+  `# Also download name translations from wikidata` \
+  --fetch-wikidata \
+  --mbtiles=output.mbtiles \
+  `# Store temporary node locations in memory` \
+  --nodemap-type=array --storage=ram
