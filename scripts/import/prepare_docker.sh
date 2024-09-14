cb9b3ba8b8f31ea2bb457ada7e66c592dd526f7e
Rinigus <rinigus.git@gmail.com>
2024-09-14 18:21:19 +0300

    Update docker based import

---
+++ scripts/import/prepare_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "Prepare directories for Docker-based import"
+
+source .env
+
+for d in \
+    $STORE_PLANET \
+    $STORE_OUTPUT \
+    $STORE_IMPORTED \
+    $STORE_MBTILES \
+    $STORE_NOMINATIM_DB \
+    $STORE_NOMINATIM_FLAT \
+    $STORE_VALHALLA \
+    $STORE_MISC
+do
+    echo Make directory $d
+    mkdir -p $d
+done
+
+echo Replace USER_ID and GROUP_ID with the current user data: `id -u`:`id -g`
+
+sed -i "/^USER_ID=/c\USER_ID=$(id -u)" .env
+sed -i "/^GROUP_ID=/c\GROUP_ID=$(id -g)" .env
